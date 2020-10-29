9ba5a6db484e70274a2ec9e26f458ecff2cfdaad
Njunu-sk <simonnjunu16@gmail.com>
2020-10-29 09:00:45 +0300

    add checkout

--- db/migrate/20201028050055_create_orders.rb
+++ db/migrate/20201028050055_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name, null: true
+      t.string :last_name, null: true
       t.decimal :sub_total, null: false, precision: 15, scale: 2
 
       t.timestamps
